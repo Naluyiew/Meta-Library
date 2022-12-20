1caac6f3fd41aa1d5aef42945c406ea8e9097551
Naluyiew <wwwylwei@163.com>
2022-12-20 23:10:26 +0800

    chore: 搭建后端环境

---
+++ meta-library/src/main/java/cn/naluyiew/metalibrary/MetaLibraryApplication.java
@@ -0,0 +1,13 @@
+package cn.naluyiew.metalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaLibraryApplication.class, args);
+    }
+
+}
---
+++ meta-library/src/test/java/cn/naluyiew/metalibrary/MetaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.naluyiew.metalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
