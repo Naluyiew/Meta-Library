88e156ee8c5ebcfa05ad339bfdc48ee52ab2ebe4
Naluyiew <wwwylwei@163.com>
2022-12-21 21:27:17 +0800

    feat: 添加登录页面

--- meta-library-vue/src/main.js
+++ meta-library-vue/src/main.js
@@ -1,12 +1,19 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+// 设置反向代理，前端请求默认发送到 http://localhost:8443/api
+const axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8443/api'
+
+// 全局注册，在其他组件中可以通过 this.$axios 发送数据
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(ElementUI)
+
 new Vue({
   el: '#app',
   router,
