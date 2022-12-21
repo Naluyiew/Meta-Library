468d8b29e1b4dea8fba767288e814a2c8231021b
Naluyiew <wwwylwei@163.com>
2022-12-21 21:23:43 +0800

    feat: 添加登录功能

---
+++ meta-library/src/main/java/cn/naluyiew/metalibrary/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package cn.naluyiew.metalibrary.dao;
+
+import cn.naluyiew.metalibrary.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDAO extends JpaRepository<User, Integer> {
+    // 通过用户名查询
+    User findByUsername(String username);
+
+    // 通过用户名及密码查询
+    User getByUsernameAndPassword(String username, String password);
+}
