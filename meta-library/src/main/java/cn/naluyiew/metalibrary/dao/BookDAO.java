ea977aba1322dfc145880a790c002335f03601f9
Naluyiew <wwwylwei@163.com>
2022-12-23 23:24:19 +0800

    feat: 完善图书查询功能

--- meta-library/src/main/java/cn/naluyiew/metalibrary/dao/BookDAO.java
+++ meta-library/src/main/java/cn/naluyiew/metalibrary/dao/BookDAO.java
@@ -9,5 +9,6 @@ import java.util.List;
 public interface BookDAO extends JpaRepository<Book, Integer> {
     List<Book> findAllByCategory(Category category);
 
+    // 根据书名或作者进行模糊查询
     List<Book> findAllByTitleLikeOrAuthorLike(String keyword1, String keyword2);
 }
