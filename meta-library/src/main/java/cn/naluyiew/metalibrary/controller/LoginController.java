8016acc7ad8ec2ccde20608de86dfe5eb8ba7d50
Naluyiew <wwwylwei@163.com>
2022-12-26 12:53:40 +0800

    fix: 完善登录功能

--- meta-library/src/main/java/cn/naluyiew/metalibrary/controller/LoginController.java
+++ meta-library/src/main/java/cn/naluyiew/metalibrary/controller/LoginController.java
@@ -1,12 +1,16 @@
 package cn.naluyiew.metalibrary.controller;
 
-import cn.naluyiew.metalibrary.entity.Result;
+import cn.naluyiew.metalibrary.result.Result;
 import cn.naluyiew.metalibrary.entity.User;
+import cn.naluyiew.metalibrary.result.ResultFactory;
 import cn.naluyiew.metalibrary.service.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class LoginController {
@@ -14,19 +18,18 @@ public class LoginController {
     @Autowired
     UserService userService;
 
-    @CrossOrigin
     @PostMapping(value = "/api/login")
     @ResponseBody
     public Result login(@RequestBody User requestUser) {
-        // 对 html 标签进行转义，防止 XSS 攻击
         String username = requestUser.getUsername();
-        username = HtmlUtils.htmlEscape(username);
-
-        User user = userService.get(username, requestUser.getPassword());
-        if (null == user) {
-            return new Result(400);
-        } else {
-            return new Result(200);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, requestUser.getPassword());
+        try {
+            subject.login(usernamePasswordToken);
+            return ResultFactory.buildSuccessResult(username);
+        } catch (AuthenticationException e) {
+            String message = "账号密码错误";
+            return ResultFactory.buildFailResult(message);
         }
     }
 }
---
+++ meta-library/src/main/java/cn/naluyiew/metalibrary/result/Result.java
@@ -0,0 +1,17 @@
+package cn.naluyiew.metalibrary.result;
+
+import lombok.Data;
+
+@Data
+public class Result {
+    //响应码
+    private int code;
+    private String message;
+    private Object result;
+
+    Result(int code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.result = data;
+    }
+}
