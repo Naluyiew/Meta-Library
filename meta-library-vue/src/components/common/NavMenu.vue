469b8980e553b03170c0bbc359ebc4fbab3ee671
Naluyiew <wwwylwei@163.com>
2022-12-30 17:23:45 +0800

    fix: 调整图书馆页面布局

--- meta-library-vue/src/components/common/NavMenu.vue
+++ meta-library-vue/src/components/common/NavMenu.vue
@@ -1,10 +1,9 @@
 <template>
-  <el-menu :default-active="'/library'" router mode="horizontal" style="min-width: 1500px">
+  <el-menu :default-active="'/library'" router mode="horizontal">
     <el-menu-item v-for="(item, i) in navList" :key="i" :index="item.name">
       {{ item.navItem }}
     </el-menu-item>
     <span id="title">Meta Library - No end for learning</span>
-    <el-button type="primary" round @click="logout" style="position: absolute;right: 5%;top: 10px;">退出登录</el-button>
   </el-menu>
 </template>
 
@@ -19,17 +18,6 @@ export default {
       ]
     }
   },
-  methods: {
-    logout() {
-      this.$axios.get('/logout').then(resp => {
-        if (resp.data.code === 200) {
-          // 前后端状态保持一致
-          this.$store.commit('logout')
-          this.$router.replace('/login')
-        }
-      })
-    }
-  }
 }
 </script>
 
@@ -40,5 +28,6 @@ export default {
   right: 35%;
   font-size: 20px;
   font-weight: bold;
+  color: #3377aa;
 }
 </style>
